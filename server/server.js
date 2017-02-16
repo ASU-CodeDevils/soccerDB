1bf7a63c5dade7f74c6d33069247ad3fdcc61af0
Evan Borchert <eborchert6@gmail.com>
2017-02-16 17:56:15 -0500

    aster

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ var express = require("express");
 var path = require("path");
 
 var app = express();
-app.set('port', (process.env.port||5000));
+app.set('port', (process.env.PORT||5000));
+
 
 app.use(express.static(path.join(__dirname, "../dist")));
 app.listen(app.get('port'), function(){
