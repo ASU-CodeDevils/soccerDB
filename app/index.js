ae018ca384b7046b5c65a42bb6da07d4b226ad0c
Evan Borchert <eborchert6@gmail.com>
2017-02-20 20:28:44 -0500

    aster

--- app/index.js
+++ app/index.js
@@ -4,6 +4,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var Griddle = require('griddle-react');
 var toDisplay;
+var check = false;
 function ajaxRequest(){//This function at the top is for avoiding browsers which can't handle ajax. 
  var activexmodes=["Msxml2.XMLHTTP", "Microsoft.XMLHTTP"] //activeX versions to check for in IE
  if (window.ActiveXObject){ //Test for support for ActiveXObject in IE first (as XMLHttpRequest in IE7 is broken)
@@ -26,13 +27,41 @@ var TestingSQL = React.createClass({
     getInitialState: function(){//React deals in states, the initial state of this component is blank. 
         return{
             input: '',
-            result: ''
+            result: '', 
+            query: '', 
+            keyword: ''
         }
     },
     updateInput: function(e){//As information is entered into the input, the state is updated as well. If you took this line out the box would be empty. 
         this.setState({
             input: e.target.value
         });
+        if(check)
+            {
+                this.showResult;
+            }
+    },
+    keywordUpdate: function(e){
+      this.setState({
+          keyword: e.target.value   
+      });  
+        
+    },
+    keywordSearch: function(){
+      var search = "SELECT DISTINCT name AS Player, position AS Position,playernumber AS Number, games_played AS Games, teamName AS Team, leagueName AS League FROM Players,Teams, Leagues WHERE ((name LIKE '%"; 
+        search +=this.state.keyword + "%' OR position LIKE '%";
+        search +=this.state.keyword + "%' OR teamName LIKE '%";
+        //search +=this.state.keyword + "%' OR leagueName LIKE '%";
+        search +=this.state.keyword + "%') AND Players.teamID=Teams.teamID AND Teams.leagueID=Leagues.leagueID)";
+        console.log(search);
+        check = true;
+         
+       this.setState({
+            input: search
+        }, function aferchange(){this.showResult()});
+       
+        console.log(this.input);
+        
     },
     showResult: function(){//This state update is to handle the query. 
         var results;//Variable to hold thre results. 
@@ -60,6 +89,7 @@ var TestingSQL = React.createClass({
     mypostrequest.open("POST", "http://sportswiz.herokuapp.com/query", true);//Giving ajax the url and type.
     mypostrequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");//Encoding.
     mypostrequest.send("query="+sendresult);//Sending our query back to our server. Which will then connect to the database and then return the result of the query.
+        check = false;
    
     },
     render: function(){
@@ -68,8 +98,12 @@ var TestingSQL = React.createClass({
             <h3>Query Database</h3><br></br>
             Submit Query: <input type="text" value={this.state.input} onChange={this.updateInput}/>
             <button onClick={this.showResult}>Query</button><br></br>
-            <p>{this.state.result}</p>
-            <Griddle results={toDisplay}/>
+            <p>Player/Position/Team Search: <input type= "text" value = {this.state.keyword} onChange={this.keywordUpdate}/><button onClick={this.keywordSearch}>Search</button></p>
+            <br></br>
+           
+            <Griddle results={toDisplay} showSettings={true}/>
+            <br></br>
+            <p>{this.state.input}</p>
             </div>
         )
     }
